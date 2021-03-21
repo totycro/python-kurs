b60e55ab71884f6e841963effbaf4ffb32c1e428
Bernhard Mallinger <bernhard.mallinger@gmx.at>
2021-03-21 11:25:34 +0100

    Chapter pandas updates

--- code-examples/machine_frame_construction.py
+++ code-examples/machine_frame_construction.py
@@ -1,7 +1,9 @@
+import pandas as pd
 machines = ["Maschine A", "Maschine K", "Maschine L"]
 thickness = pd.Series([4.3, 1.2, 2.2], index=machines)
 speed = pd.Series([100, 50, 80], index=machines)
-machines_frame = pd.concat(
+machines_frame = pd.DataFrame(
     {'speed': speed, 'thickness': thickness},
-    axis='columns',
 )
+
+print(machines_frame)
